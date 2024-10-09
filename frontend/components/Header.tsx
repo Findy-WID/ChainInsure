6c124bcdb693c966458b1a31233d3d0d61c8fe42
Divine Success <divinesuccess228@gmail.com>
2024-10-09 15:10:51 +0100

    update landing page

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -2,9 +2,10 @@
 
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
-import { ShieldCheck } from 'lucide-react'
+import { ShieldCheck, ArrowRight } from 'lucide-react'
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
 import { Button } from "@/components/ui/button"
+
 
 export function Header() {
   const { isConnected } = useAccount()
@@ -21,23 +22,35 @@ export function Header() {
   }, [])
 
   return (
-    <header className={`fixed w-full transition-all duration-300 ${isScrolled ? 'bg-white/80 backdrop-blur-md shadow-md' : ''}`}>
+    <header className={`fixed bg-[#15173033] w-full transition-all duration-300 ${isScrolled ? 'bg-white/80 backdrop-blur-md shadow-md' : ''}`}>
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <Link href="/" className="flex items-center space-x-2">
-          <ShieldCheck className="h-8 w-8 text-blue-600" />
-          <span className="text-2xl font-serif font-bold">Chain Insure</span>
-        </Link>
+
         <nav className="hidden md:flex space-x-8">
-          <Link href="#features" className="text-gray-600 hover:text-blue-600">Features</Link>
-          <Link href="#pricing" className="text-gray-600 hover:text-blue-600">Pricing</Link>
-          <Link href="#contact" className="text-gray-600 hover:text-blue-600">Contact</Link>
+          <Link href="#docs" className="text-white hover:text-blue-600">Docs</Link>
+          <Link href="#about-us" className="text-white hover:text-blue-600">About us</Link>
+          <Link href="#blog" className="text-white hover:text-blue-600">Blog</Link>
+          <Link href="#support" className="text-white hover:text-blue-600">Support</Link>
         </nav>
+        <Link href="/" className="flex items-center space-x-2">
+          {/* <ShieldCheck className="h-8 w-8 text-blue-600" /> */}
+          <span className="text-2xl font-light leading-10 text-white font-serif">Chain Insure</span>
+        </Link>
+
+        <div className='flex justify-between'>
+        <Button className='bg-slate-600 rounded-full text-white mr-4'>Get the app <ArrowRight className='ml-2 self-center'/></Button>
+        
         {isConnected ? (
-          <Button onClick={() => disconnect()}>Disconnect</Button>
+          <Button className = 'bg-white text-black rounded-full' onClick={() => disconnect()}>Disconnect</Button>
         ) : (
-          <Button onClick={() => connect({ connector: connectors[0] })}>Connect Wallet</Button>
+          <Button className = 'bg-white text-black rounded-full' onClick={() => connect({ connector: connectors[0] })}>Connect wallet</Button>
         )}
+        </div>
+
+        
       </div>
     </header>
   )
 }
+
+
+
